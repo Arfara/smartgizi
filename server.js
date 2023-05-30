9013c43b296d1fa183325c585fbe04a80178d625
Arfara Yema S <indonesianstudent14@gmail.com>
2023-05-30 22:14:21 +0700

    dasdas

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
