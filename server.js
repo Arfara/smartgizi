957d602558f11399b560b0512df2e74ac002fd57
Arfara Yema S <indonesianstudent14@gmail.com>
2023-05-30 20:22:52 +0700

    new refreshment code

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import db from "./config/database.js";
+import router from "./routes/index.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+dotenv.config();
+const app=express();
+
+try {
+    await db.authenticate();
+    console.log('database connected');
+} catch (error) {
+    console.log(error); 
+}
+
+app.use(cors({credentials:true,origin:'*'}));
+app.use(cookieParser());
+app.use(express.json());
+app.use(router);
+
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT,()=>console.log(`Server running at port ${PORT}`));
