caaac00ca357f3ecf542d8d50bea6a0780dbad6e
Arfara Yema S <71955832+Arfara@users.noreply.github.com>
2023-06-08 13:11:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import db from "./config/database.js";
 import router from "./routes/index.js";
 import dotenv from "dotenv";
 import cors from "cors";
-import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
 
@@ -15,7 +14,6 @@ try {
 }
 
 app.use(cors({ credentials: true, origin: '*' }));
-app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
