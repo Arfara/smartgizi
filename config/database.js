8f26562afb78cf2f009213cdbe2c294c1b01c6ad
Arfara Yema S <71955832+Arfara@users.noreply.github.com>
2023-06-14 22:13:44 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('smartgizi-user-db','root','' {
-    host:'34.101.41.22',
+const db = new Sequelize('smartgizidb','root','smartgizisql123' {
+    host:'34.128.70.34',
     dialect:'mysql'
 });
 
